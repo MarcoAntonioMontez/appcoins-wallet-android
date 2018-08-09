4c19d49fd5fac90320593052ab9daf0d1d5d0f65
Marco Montez <marco.montez.lis@gmail.com>
2018-08-09 17:32:57 +0100

    Added Wheel View/Presenter contract

--- quiz/src/main/java/com/example/quiz/wheel/WheelContract.java
+++ quiz/src/main/java/com/example/quiz/wheel/WheelContract.java
@@ -1,5 +1,15 @@
 package com.example.quiz.wheel;
 
 public interface WheelContract {
+    interface WheelView{
+        void onWheelButtonClicked();
+        void onNextButtonClicked();
+        void displayReward(String str);
+    }
+
+    interface WheelPresenter{
+        void loadWheel();
+        void spinWheel();
+    }
 
 }
